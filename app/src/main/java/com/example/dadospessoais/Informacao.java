0047882c0a3ff54db15e8436d349e2710486ce58
DanielSiob <48354047+danielsiob@users.noreply.github.com>
2020-04-03 19:34:43 +0100

    Mostrar informações no novo layout

--- app/src/main/java/com/example/dadospessoais/Informacao.java
+++ app/src/main/java/com/example/dadospessoais/Informacao.java
@@ -2,7 +2,9 @@ package com.example.dadospessoais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Informacao extends AppCompatActivity {
 
@@ -10,5 +12,34 @@ public class Informacao extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacao);
+
+        Intent intentRecebe = getIntent();
+
+        Bundle parametros = intentRecebe.getExtras();
+
+
+
+        String nome = parametros.getString("chave_nome");
+        String idade = parametros.getString("chave_idade");
+        String telefone = parametros.getString("chave_telefone");
+        String email = parametros.getString("chave_email");
+        String peso = parametros.getString("chave_peso");
+        String altura = parametros.getString("chave_altura");
+
+
+        TextView textViewNome = (TextView)findViewById(R.id.textViewVerNome);
+        TextView textViewIdade = (TextView)findViewById(R.id.textViewVerIdade);
+        TextView textViewTelefone = (TextView)findViewById(R.id.textViewVerTelefone);
+        TextView textViewEmail = (TextView)findViewById(R.id.textViewVerEmail);
+        TextView textViewPeso = (TextView)findViewById(R.id.textViewVerPeso);
+        TextView textViewAltura = (TextView)findViewById(R.id.textViewVerAltura);
+
+
+        textViewNome.setText(nome);
+        textViewIdade.setText(idade);
+        textViewTelefone.setText(telefone);
+        textViewEmail.setText(email);
+        textViewPeso.setText(peso);
+        textViewAltura.setText(altura);
     }
 }
--- app/src/main/java/com/example/dadospessoais/MainActivity.java
+++ app/src/main/java/com/example/dadospessoais/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.dadospessoais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNome;
+    private EditText editTextIdade;
+    private EditText editTextTelefone;
+    private EditText editTextEmail;
+    private EditText editTextPeso;
+    private EditText editTextAltura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextNome = (EditText) findViewById(R.id.editTextNome);
+        editTextTelefone = (EditText) findViewById(R.id.editTextTelefone);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextIdade = (EditText) findViewById(R.id.editTextIdade);
+        editTextPeso = (EditText) findViewById(R.id.editTextPeso);
+        editTextAltura = (EditText) findViewById(R.id.editTextAltura;
+    }
+
+    public void onClickBotaoEnviar(View view){
+        String nome = editTextNome.getText().toString();
+        String idade = editTextIdade.getText().toString();
+        String telefone = editTextTelefone.getText().toString();
+        String email = editTextEmail.getText().toString();
+        String peso = editTextPeso.getText().toString();
+        String altura = editTextAltura.getText().toString();
+
+        Intent intentEnviar = new Intent(getApplicationContext(), Informacao.class);
+        Bundle parametros = new Bundle();
+
+
+        parametros.putString("chave_nome", nome);
+        parametros.putString("chave_idade", idade);
+        parametros.putString("chave_telefone", telefone);
+        parametros.putString("chave_email", email);
+        parametros.putString("chave_peso", peso);
+        parametros.putString("chave_altura", altura);
+
+        intentEnviar.putExtras(parametros);
+
+        startActivity(intentEnviar);
     }
 }
